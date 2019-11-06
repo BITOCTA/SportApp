3270034929409faba5d8fec1a754f3fdc0d3f76d
BITOCTA <artso003@gmail.com>
2019-11-06 16:14:17 +0100

    added start fragment and navigation

--- app/src/main/java/com/bitocta/sportapp/ui/ProgressFragment.java
+++ app/src/main/java/com/bitocta/sportapp/ui/ProgressFragment.java
@@ -1,4 +1,68 @@
 package com.bitocta.sportapp.ui;
 
-public class ProgressFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.bitocta.sportapp.R;
+import com.google.android.material.navigation.NavigationView;
+
+
+public class ProgressFragment extends Fragment {
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView nv;
+    private Toolbar toolbar;
+
+
+    public ProgressFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static ProgressFragment getInstance() {
+        ProgressFragment fragment = new ProgressFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(getActivity(), mDrawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_progress, container, false);
+
+
+        mDrawerLayout = view.findViewById(R.id.drawer_layout);
+        nv = view.findViewById(R.id.navigation);
+        toolbar = view.findViewById(R.id.z_toolbar);
+
+        return view;
+    }
+
+
 }
--- app/src/main/java/com/bitocta/sportapp/ui/StartFragment.java
+++ app/src/main/java/com/bitocta/sportapp/ui/StartFragment.java
@@ -4,16 +4,30 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.bitocta.sportapp.R;
+import com.google.android.material.button.MaterialButton;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+
+import java.util.Calendar;
 
 
-public class StartFragment extends Fragment {
+public class StartFragment extends Fragment implements DatePickerDialog.OnDateSetListener{
+
+    private EditText editName;
+    private EditText editSex;
+    private EditText editHeight;
+    private EditText editWeight;
+    private EditText editDateOfBirth;
+    private MaterialButton doneButton;
 
 
     public StartFragment() {
@@ -27,18 +41,60 @@ public class StartFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        editDateOfBirth.setOnClickListener(mView -> {
+
+            Calendar now = Calendar.getInstance();
+            DatePickerDialog dpd = DatePickerDialog.newInstance(
+                    StartFragment.this,
+                    now.get(Calendar.YEAR),
+                    now.get(Calendar.MONTH),
+                    now.get(Calendar.DAY_OF_MONTH)
+
+            );
 
 
 
+            dpd.show(getFragmentManager(), "Datepickerdialog");
+        });
 
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, ProgressFragment.getInstance()).commit();
+            }
+        });
     }
+
+
+
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_start, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_start, container, false);
+        editName = view.findViewById(R.id.edit_user_name);
+        editSex = view.findViewById(R.id.edit_user_sex);
+        editHeight = view.findViewById(R.id.edit_user_height);
+        editWeight = view.findViewById(R.id.edit_user_weight);
+        editDateOfBirth = view.findViewById(R.id.edit_user_date_of_birth);
+        doneButton = view.findViewById(R.id.button_done);
+        return view;
     }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        String date;
+        if (monthOfYear >= 9)
+            date = dayOfMonth + "." + (monthOfYear + 1) + "." + year;
+        else
+            date = dayOfMonth + ".0" + (monthOfYear + 1) + "." + year;
+
+        editDateOfBirth.setText(date);
+    }
+
 }
